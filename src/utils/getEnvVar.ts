9413388e00e87f405cce0bda0ddaa021606f5b0e
Oleg <olegkron.cc@gmail.com>
2025-07-22 15:55:57 +0100

    upd

--- src/utils/getEnvVar.ts
+++ src/utils/getEnvVar.ts
@@ -1,10 +1,7 @@
-import { Address } from "viem";
 import process from "process";
-import { type env } from "../types/env";
 
-
-export function getEnvVar<K extends keyof env>(key: K): env[K] {
+export function getEnvVar(key: string): string {
     const value = process.env[key];
     if (value === undefined || value === "") throw new Error(`Missing environment variable ${key}`);
-    return value as env[K];
+return value;
 }
